ec22340845fce17fb19f5460d5d4e80647909a48
blackdanamite <seagrendaniel562@gmail.com>
2020-03-10 16:57:26 -0700

    initial setup of SearchBar component

---
+++ src/Components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+import VideoItem from './VideoItem';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
