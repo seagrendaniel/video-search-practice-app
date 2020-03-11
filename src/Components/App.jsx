c013db2ff81539062784194092b3b33d085a7444
blackdanamite <seagrendaniel562@gmail.com>
2020-03-10 18:04:35 -0700

    create VideoList with VideoItems and style

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,10 +3,9 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
-import VideoItem from './VideoItem';
 
 class App extends React.Component {
-  state = {videos: []};
+  state = { videos: [] };
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -15,14 +14,14 @@ class App extends React.Component {
       }
     });
 
-    this.setState({videos: response.data.items});
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit}/>
-        I have {this.state.videos.length} videos
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
--- src/Components/VideoItem.jsx
+++ src/Components/VideoItem.jsx
@@ -1,1 +1,20 @@
 import React from 'react';
+
+const VideoItem = ({ video }) => {
+  return (
+    <div className="item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt=""
+      />
+      <div className="content">
+        <div className="header">
+          {video.snippet.title}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default VideoItem;
